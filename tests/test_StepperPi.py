bc164874b2f005cc280165667bb7c8cf8982a44f
Happyday <happyday.mjohnson@gmail.com>
2022-06-16 05:54:14 -0700

    evolved

--- tests/test_StepperPi.py
+++ tests/test_StepperPi.py
@@ -3,15 +3,19 @@ from StepperPi import Stepper
 
 
 @pytest.fixture
-def version():
-    s = Stepper(8,15,18,23,24)
-    version = s.version()
-    return version
+def myStepper():
+    myStepper = Stepper(8,15,18,23,24)
+    return myStepper
 
-def test_version_number(version):
+def test_version_number(myStepper):
     # This test was run on version 0. Versions are integer numbers
-    assert version == 0
+    assert myStepper.version() == 0
     
 
-def test_version_type(version):
-    assert isinstance(version,int)
+def test_version_type(myStepper):
+    assert isinstance(myStepper.version(),int)
+
+def test_set_speed(myStepper):
+    step_delay_at_15RPM =  60 * 1000 * 1000 /myStepper.number_of_steps / 15
+    myStepper.setSpeed(15)
+    assert myStepper.step_delay == step_delay_at_15RPM
