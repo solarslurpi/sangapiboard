15a1b300a6af65f23142c49dace124bb356871ee
Happyday <happyday.mjohnson@gmail.com>
2022-06-20 05:34:39 -0700

    evolved testing.

--- tests/test.py
+++
@@ -1,4 +0,0 @@
-from StepperPi import Stepper
-
-s = Stepper(8,15,18,23,24)
-s.step(5)
--- tests/test_StepperPi.py
+++ tests/test_StepperPi.py
@@ -1,14 +1,16 @@
+import time
 import pytest
 from StepperPi import Stepper
 
 
 @pytest.fixture
 def myStepper():
-    myStepper = Stepper(8,15,18,23,24)
+    myStepper = Stepper(8,12, 15, 11, 13)
     return myStepper
+    
 
 def test_version_number(myStepper):
-    # This test was run on version 0. Versions are integer numbers
+    # This test was run on version 0. Versions are integer numbers.
     assert myStepper.version() == 0
     
 
@@ -19,3 +21,11 @@ def test_set_speed(myStepper):
     step_delay_at_15RPM =  60 * 1000 * 1000 /myStepper.number_of_steps / 15
     myStepper.setSpeed(15)
     assert myStepper.step_delay == step_delay_at_15RPM
+
+def test_one_rotation(myStepper):
+    fWaitTime = 8 / float(1000)
+    for i in range(4096):
+        myStepper.stepMotor(i % 8)
+        time.sleep(fWaitTime)
+
+
